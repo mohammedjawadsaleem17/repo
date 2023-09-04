264386a4cd1d22588bb2e2aa1701d6ec2a0b9ffa
Mohammed Jawad Saleem <mohammedjawadsaleem11@gmail.com>
2023-09-04 19:39:41 +0530

    Some Stash File

--- dev.py
+++ dev.py
@@ -1,4 +1,5 @@
 print("Hello World")
+<<<<<<< Updated upstream
 for i in range(10):
     if i%2==0:
         print(f"{i} is a Even Number")
@@ -6,3 +7,7 @@ print("Execution Completed")
 1
 
 
+=======
+for _ in range(100):
+    print("Done"
+>>>>>>> Stashed changes
